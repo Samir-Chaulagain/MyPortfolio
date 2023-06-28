b30e9c80576d3e169745b8138de91c66c2be6793
Samir <royal.chaulagain321@gmail.com>
2023-06-29 05:07:26 +0545

    Added CV button

--- app.js
+++ app.js
@@ -11,3 +11,24 @@
     document.body.classList.toggle("light-mode");
   });
 })();
+
+
+document
+  .getElementById("downloadButton")
+  .addEventListener("click", function () {
+    // Create an anchor element
+    var link = document.createElement("a");
+
+    link.href = "path/to/file.pdf"; // Replace with the actual file URL
+    // Set the file name
+    link.download = ".Profile.pdf"; // Replace with the desired file name
+
+    // Append the anchor element to the document body
+    document.body.appendChild(link);
+
+    // Simulate a click on the anchor element
+    link.click();
+
+    // Remove the anchor element from the document body
+    document.body.removeChild(link);
+  });
