bb8e768fc66e9fe49be838bd1f60a9330dadc648
Samir Chaulagain <89231471+Samir-Chaulagain@users.noreply.github.com>
2024-09-02 08:30:04 +0545

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
