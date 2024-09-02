717641e6e34acd541b3f267b7436cdce4d27f91b
Samir Chaulagain <89231471+Samir-Chaulagain@users.noreply.github.com>
2024-09-02 10:25:40 +0545

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -124,7 +124,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # This is where collectstatic will gather files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # This is where collectstatic will gather files
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # This is the directory where your static files are stored
 ]
